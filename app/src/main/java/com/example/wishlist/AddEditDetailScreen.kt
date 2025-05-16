4cb46790dba7194ab1835d7fdd55b8a0f6acfc3f
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-17 00:30:05 +0900

    Designing our own OutlinedTextField

--- app/src/main/java/com/example/wishlist/AddEditDetailScreen.kt
+++ app/src/main/java/com/example/wishlist/AddEditDetailScreen.kt
@@ -3,13 +3,23 @@ package com.example.wishlist
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -34,7 +44,53 @@ fun AddEditDetailScreen(
             verticalArrangement = Arrangement.Center,
         ) {
             Spacer(modifier = Modifier.height(10.dp))
-
+            WishTextField(
+                label = "Label",
+                value = "Value",
+                onValueChange = {},
+            )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(
+                text = label,
+                color = Color.Black,
+            )
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            // Using predefined color
+            focusedTextColor = Color.Black,
+            unfocusedTextColor = Color.Black,
+            // Using color in res.values.colors
+            focusedBorderColor = colorResource(id = R.color.black),
+            unfocusedBorderColor = colorResource(id = R.color.black),
+            cursorColor = colorResource(id = R.color.black),
+            focusedLabelColor = colorResource(id = R.color.black),
+            unfocusedLabelColor = colorResource(id = R.color.black),
+        ),
+        modifier = Modifier.fillMaxWidth(),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WishTextFieldPreview() {
+    WishTextField(
+        label = "Label",
+        value = "Value",
+        onValueChange = {},
+    )
 }
