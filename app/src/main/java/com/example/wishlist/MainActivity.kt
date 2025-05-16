20350997aed7c7a8cfb62a87931b37eefb617484
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-17 00:09:19 +0900

    Preparing for Navigation

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         // enableEdgeToEdge()
         setContent {
             WishListTheme {
-                HomeScreen()
+                Navigation()
             }
         }
     }
---
+++ app/src/main/java/com/example/wishlist/WishViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.wishlist
+
+import androidx.lifecycle.ViewModel
+
+class WishViewModel : ViewModel() {
+
+
+}
