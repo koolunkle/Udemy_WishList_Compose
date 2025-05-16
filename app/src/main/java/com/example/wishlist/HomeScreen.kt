b5c6c52188de460cd2afa879fcb643434b78fda2
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-16 23:50:59 +0900

    FloatingActionButton (FAB)

--- app/src/main/java/com/example/wishlist/HomeScreen.kt
+++ app/src/main/java/com/example/wishlist/HomeScreen.kt
@@ -1,13 +1,20 @@
 package com.example.wishlist
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
@@ -20,12 +27,26 @@ fun HomeScreen() {
                     Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
                 },
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                )
+            }
         }
     ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .background(color = Color.White),
         ) {
 
         }
