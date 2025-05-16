1003029f94296246f110e308c118830e944a62df
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-16 23:59:47 +0900

    Adding our Wish and WishItem

--- app/src/main/java/com/example/wishlist/HomeScreen.kt
+++ app/src/main/java/com/example/wishlist/HomeScreen.kt
@@ -2,18 +2,25 @@ package com.example.wishlist
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -48,7 +55,41 @@ fun HomeScreen() {
                 .padding(paddingValues)
                 .background(color = Color.White),
         ) {
+            items(3) {
+                WishItem(
+                    wish = Wish(
+                        id = it.toLong(),
+                        title = "Wish $it",
+                        description = "Description $it",
+                    ),
+                    onClick = {},
+                )
+            }
+        }
+    }
+}
 
+@Composable
+fun WishItem(
+    wish: Wish,
+    onClick: () -> Unit,
+) {
+    Card(
+        colors = CardDefaults.elevatedCardColors(containerColor = Color.DarkGray),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+            .clickable { onClick() },
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = wish.title,
+                fontWeight = FontWeight.ExtraBold,
+            )
+            Text(
+                text = wish.description,
+                fontWeight = FontWeight.ExtraBold,
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/wishlist/Wish.kt
@@ -0,0 +1,7 @@
+package com.example.wishlist
+
+data class Wish(
+    val id: Long = 0L,
+    val title: String = "",
+    val description: String = "",
+)
